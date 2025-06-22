f13806ccea1e8349d0ae81b179328f792206945a
Joey Liao <joey01271998@gmail.com>
2025-06-22 20:31:50 +0800

    Refactor: Rename FE and BE folder name.

---
+++ BE/SpringBoot/src/main/java/com/template/template/controller/SampleController.java
@@ -0,0 +1,12 @@
+package com.template.template.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SampleController {
+    @GetMapping("/sample")
+    public String sample(String[] args) {
+        return "sample";
+    }
+}
