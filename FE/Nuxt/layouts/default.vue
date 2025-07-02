5a32dae96d534325bc41be1cf782248c69368c4c
Joey Liao <joey01271998@gmail.com>
2025-07-02 16:55:57 +0800

    Feat: Setting tailwind config and add sample buttons.

--- FE/Nuxt/layouts/default.vue
+++ FE/Nuxt/layouts/default.vue
@@ -1,9 +1,7 @@
 <template>
-	<div class="page">
-		<p>現在時間 {{ dayjs().format("YYYY/MM/DD") }}</p>
-		<FormInput></FormInput>
-		<slot />
-	</div>
+    <div class="page">
+        <slot />
+    </div>
 </template>
 <script setup lang="ts">
 import dayjs from "dayjs";
--- FE/Nuxt/pages/sample.vue
+++ FE/Nuxt/pages/sample.vue
@@ -1,11 +1,17 @@
 <template>
-    <div class="prose">
-        <h1 class="text-2xl">Sample Page</h1>
-        <p>This is sample page.</p>
-        <UButton icon="i-lucide-sun" variant="subtle">Button</UButton>
+    <div class="p-8 space-y-6">
+        <h1 class="text-3xl font-bold text-primary">Sample Color Test</h1>
+
+        <USeparator icon="i-lucide-pencil"></USeparator>
+        <div class="space-x-4">
+            <h2 class="text-white pb-4 font-medium text-2xl">Buttons</h2>
+            <div class="flex gap-2">
+                <UButton class="bg-primary text-white px-4 py-2 rounded shadow hover:bg-primary/90">Primary</UButton>
+                <UButton class="bg-accent text-white px-4 py-2 rounded shadow hover:bg-accent/90">Accent</UButton>
+            </div>
+            <h3>Button with icon</h3>
+
+            <UButton icon="i-lucide-sun">accessibility</UButton>
+        </div>
     </div>
 </template>
-
-<script setup lang="ts"></script>
-
-<style></style>
